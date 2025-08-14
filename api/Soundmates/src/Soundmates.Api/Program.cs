c3af0370feda85865d859bc1bd51f14b5c04c103
michal-dudniczenko <dudniczenkom@gmail.com>
2025-08-14 16:44:15 +0200

    api project setup changes

--- api/Soundmates/src/Soundmates.Api/Program.cs
+++ api/Soundmates/src/Soundmates.Api/Program.cs
@@ -1,17 +1,33 @@
 using Microsoft.EntityFrameworkCore;
+using Soundmates.Domain.Interfaces.Auth;
+using Soundmates.Infrastructure.Auth;
 using Soundmates.Infrastructure.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    throw new InvalidOperationException("Database connection string is not configured.");
+}
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found."));
+    options.UseNpgsql(connectionString);
 });
+
+var secretKey = builder.Configuration["SecretKey"];
+
+if (string.IsNullOrEmpty(secretKey))
+{
+    throw new InvalidOperationException("Secret key is not configured.");
+}
+
+builder.Services.AddSingleton<IAuthService>(new AuthService(secretKey));
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-
-// TODO add identity
 
 var app = builder.Build();
 
@@ -22,9 +38,13 @@ if (app.Environment.IsDevelopment())
     {
         options.SwaggerEndpoint("/openapi/v1.json", "Soundmates API");
     });
-}
-app.UseHttpsRedirection();
 
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
+
+app.UseHttpsRedirection();
 app.MapControllers();
 
 await app.RunAsync();
