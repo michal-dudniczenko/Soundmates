e58db95589cc4a65e6953df4c9297f1c69d44885
michal-dudniczenko <dudniczenkom@gmail.com>
2025-09-06 23:08:10 +0200

    add api extension for mapping result -> http responses + logging responses, global exception handler, middleware to log requests

--- api/Soundmates/src/Soundmates.Api/Extensions/ControllerExtensions.cs
+++ api/Soundmates/src/Soundmates.Api/Extensions/ControllerExtensions.cs
@@ -1,29 +1,99 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Soundmates.Domain.Entities;
-using Soundmates.Domain.Interfaces.Repositories;
-using System.Security.Claims;
+using Soundmates.Application.Common;
 
 namespace Soundmates.Api.Extensions;
 
 public static class ControllerExtensions
 {
-    public static async Task<User?> GetAuthorizedUserAsync(
+    public static IActionResult ResultToHttpResponse<T>(
         this ControllerBase controller,
-        IUserRepository userRepository,
-        bool checkForFirstLogin = true)
+        Result<T> result)
     {
-        var subClaim = controller.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        if (subClaim is null || !int.TryParse(subClaim, out var authorizedUserId))
+        var logger = controller.HttpContext.RequestServices
+            .GetRequiredService(typeof(ILogger<>)
+            .MakeGenericType(controller.GetType()))
+            as ILogger;
+
+        IActionResult response;
+        int statusCode;
+
+        if (result.IsSuccess)
         {
-            return null;
+            response = controller.Ok(result.Value);
+            statusCode = 200;
+        }
+        else
+        {
+            (response, statusCode) = result.ErrorType switch
+            {
+                ErrorType.BadRequest => (controller.BadRequest(new { message = result.ErrorMessage }), 400),
+                ErrorType.Unauthorized => (controller.Unauthorized(new { message = result.ErrorMessage }), 401),
+                ErrorType.NotFound => (controller.NotFound(new { message = result.ErrorMessage }), 404),
+                ErrorType.InternalServerError or _ => (controller.StatusCode(500, new { message = result.ErrorMessage }), 500)
+            };
         }
 
-        var user = await userRepository.GetByIdAsync(authorizedUserId);
-        if (user == null || user.IsLoggedOut || (checkForFirstLogin && user.IsFirstLogin))
+        if (statusCode != 200)
         {
-            return null;
+            logger?.LogInformation("RESPONSE: {StatusCode} for {Controller}.{Action}: {ErrorMessage}",
+                statusCode,
+                controller.GetType().Name,
+                controller.ControllerContext.ActionDescriptor.ActionName,
+                result.ErrorMessage);
+        }
+        else
+        {
+            logger?.LogInformation("RESPONSE: {StatusCode} for {Controller}.{Action}",
+                statusCode,
+                controller.GetType().Name,
+                controller.ControllerContext.ActionDescriptor.ActionName);
+        }
+        return response;
+    }
+
+    public static IActionResult ResultToHttpResponse(
+        this ControllerBase controller,
+        Result result)
+    {
+        var logger = controller.HttpContext.RequestServices
+            .GetRequiredService(typeof(ILogger<>)
+            .MakeGenericType(controller.GetType()))
+            as ILogger;
+
+        IActionResult response;
+        int statusCode;
+
+        if (result.IsSuccess)
+        {
+            response = controller.Ok();
+            statusCode = 200;
+        }
+        else
+        {
+            (response, statusCode) = result.ErrorType switch
+            {
+                ErrorType.BadRequest => (controller.BadRequest(new { message = result.ErrorMessage }), 400),
+                ErrorType.Unauthorized => (controller.Unauthorized(new { message = result.ErrorMessage }), 401),
+                ErrorType.NotFound => (controller.NotFound(new { message = result.ErrorMessage }), 404),
+                ErrorType.InternalServerError or _ => (controller.StatusCode(500, new { message = result.ErrorMessage }), 500)
+            };
         }
 
-        return user;
-    } 
+        if (statusCode != 200)
+        {
+            logger?.LogInformation("RESPONSE: {StatusCode} for {Controller}.{Action}: {ErrorMessage}",
+                statusCode,
+                controller.GetType().Name,
+                controller.ControllerContext.ActionDescriptor.ActionName,
+                result.ErrorMessage);
+        }
+        else
+        {
+            logger?.LogInformation("RESPONSE: {StatusCode} for {Controller}.{Action}",
+                statusCode,
+                controller.GetType().Name,
+                controller.ControllerContext.ActionDescriptor.ActionName);
+        }
+        return response;
+    }
 }
---
+++ api/Soundmates/src/Soundmates.Api/Handlers/GlobalExceptionHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace Soundmates.Api.Handlers;
+
+public class GlobalExceptionHandler(
+    ILogger<GlobalExceptionHandler> _logger
+) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "Unhandled exception occurred while processing the request.");
+
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        httpContext.Response.ContentType = "application/json";
+
+        var response = new { message = "Something went wrong." };
+        await httpContext.Response.WriteAsJsonAsync(response, cancellationToken);
+
+        return true;
+    }
+}
