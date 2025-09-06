e0218c297fab8c4f1d181fda32ff2d668b066434
michal-dudniczenko <dudniczenkom@gmail.com>
2025-09-06 23:09:32 +0200

    add domain exceptions classes for changing display order of user media

---
+++ api/Soundmates/src/Soundmates.Domain/Exceptions/MusicSamples/DisplayOrderAlreadyFirstException.cs
@@ -0,0 +1,18 @@
+﻿namespace Soundmates.Domain.Exceptions.MusicSamples;
+
+public class DisplayOrderAlreadyFirstException : DomainException
+{
+    public DisplayOrderAlreadyFirstException()
+    {
+    }
+
+    public DisplayOrderAlreadyFirstException(string message)
+        : base(message)
+    {
+    }
+
+    public DisplayOrderAlreadyFirstException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
---
+++ api/Soundmates/src/Soundmates.Domain/Exceptions/MusicSamples/DisplayOrderAlreadyLastException.cs
@@ -0,0 +1,18 @@
+﻿namespace Soundmates.Domain.Exceptions.MusicSamples;
+
+public class DisplayOrderAlreadyLastException : DomainException
+{
+    public DisplayOrderAlreadyLastException()
+    {
+    }
+
+    public DisplayOrderAlreadyLastException(string message)
+        : base(message)
+    {
+    }
+
+    public DisplayOrderAlreadyLastException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
