73919398753625861eea3a33b81710cb9bc63688
michal-dudniczenko <dudniczenkom@gmail.com>
2025-10-23 16:07:43 +0200

    add unique constraints db migration

---
+++ api/Soundmates/src/Soundmates.Infrastructure/Migrations/20251023140643_AddUniqueConstraints.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Soundmates.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Likes_GiverId",
+                table: "Likes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Dislikes_GiverId",
+                table: "Dislikes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_GiverId_ReceiverId",
+                table: "Likes",
+                columns: new[] { "GiverId", "ReceiverId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dislikes_GiverId_ReceiverId",
+                table: "Dislikes",
+                columns: new[] { "GiverId", "ReceiverId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Likes_GiverId_ReceiverId",
+                table: "Likes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Dislikes_GiverId_ReceiverId",
+                table: "Dislikes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_GiverId",
+                table: "Likes",
+                column: "GiverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dislikes_GiverId",
+                table: "Dislikes",
+                column: "GiverId");
+        }
+    }
+}
