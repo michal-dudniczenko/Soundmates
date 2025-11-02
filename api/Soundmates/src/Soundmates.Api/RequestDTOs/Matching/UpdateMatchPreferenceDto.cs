aeece9b0c1e403dd4d0607b87fd169feaf678794
banacchini <116755374+banacchini@users.noreply.github.com>
2025-11-02 17:08:06 +0100

    match preferences Dto fix

--- api/Soundmates/src/Soundmates.Api/RequestDTOs/Matching/UpdateMatchPreferenceDto.cs
+++ api/Soundmates/src/Soundmates.Api/RequestDTOs/Matching/UpdateMatchPreferenceDto.cs
@@ -10,30 +10,21 @@ public class UpdateMatchPreferenceDto
     [Required]
     public required bool ShowBands { get; set; }
 
-    [Required]
     public required int? MaxDistance { get; set; }
 
-    [Required]
     public required Guid? CountryId { get; set; }
 
-    [Required]
     public required Guid? CityId { get; set; }
 
-    [Required]
     public required int? ArtistMinAge { get; set; }
 
-    [Required]
     public required int? ArtistMaxAge { get; set; }
 
-    [Required]
     public required Guid? ArtistGenderId { get; set; }
 
-    [Required]
     public required int? BandMinMembersCount { get; set; }
 
-    [Required]
     public required int? BandMaxMembersCount { get; set; }
 
-    [Required]
     public required IList<Guid> FilterTagsIds { get; set; }
 }
