69dd32371a64af49c145b1412ba3c67f623ad921
michal-dudniczenko <dudniczenkom@gmail.com>
2025-11-05 10:37:13 +0100

    update domain entity classes, add required modifiers

--- api/Soundmates/src/Soundmates.Domain/Entities/City.cs
+++ api/Soundmates/src/Soundmates.Domain/Entities/City.cs
@@ -4,8 +4,8 @@ public class City
 {
     public Guid Id { get; set; } = Guid.CreateVersion7();
     public required string Name { get; set; }
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
+    public required double Latitude { get; set; }
+    public required double Longitude { get; set; }
 
     public Guid CountryId { get; set; }
     public Country Country { get; set; } = null!;
--- api/Soundmates/src/Soundmates.Domain/Entities/UserMedia.cs
+++ api/Soundmates/src/Soundmates.Domain/Entities/UserMedia.cs
@@ -8,7 +8,7 @@ public abstract class UserMedia
 
     [MaxLength(100)]
     public required string FileName { get; set; }
-    public int DisplayOrder { get; set; }
+    public required int DisplayOrder { get; set; }
 
     public Guid UserId { get; set; }
     public User User { get; set; } = null!;
